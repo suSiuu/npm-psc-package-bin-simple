8b5c704c033208f6f17aedb96401b65863042798
suSiuu <151724940+suSiuu@users.noreply.github.com>
2023-12-04 13:28:20 +0800

    Update install.js

--- install.js
+++ install.js
@@ -3,17 +3,24 @@
 const https = require("follow-redirects").https;
 const tar = require("tar");
 const shell = require("shelljs");
+const HttpsProxyAgent = require("https-proxy-agent");
 const version = require("./package.json")["psc-package-version"];
 const platform = { win32: "win64", darwin: "macos" }[process.platform] || "linux64";
 
-https.get(
-  `https://github.com/purescript/psc-package/releases/download/${version}/${platform}.tar.gz`,
-  res => res.pipe(
-      tar.x({"C": 'psc-package', strip: 1}).on("finish", () => {
-        if (shell.test("-f", "./psc-package/psc-package")) {
-          shell.mv("./psc-package/psc-package", "./psc-package/psc-package.exe")
-        }
-      })
-    )
-);
+// agent's address
+const proxy = 'http://172.23.240.1:7890';  // 替换为你的代理地址和端口
+const agent = new HttpsProxyAgent(proxy);
 
+https.get({
+  hostname: 'github.com',
+  port: 443,
+  path: `/purescript/psc-package/releases/download/${version}/${platform}.tar.gz`,
+  agent: agent  // 使用代理代理请求
+}, res => res.pipe(
+    tar.x({"C": 'psc-package', strip: 1}).on("finish", () => {
+      if (shell.test("-f", "./psc-package/psc-package")) {
+        shell.mv("./psc-package/psc-package", "./psc-package/psc-package.exe")
+      }
+    })
+  )
+);
