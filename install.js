6aa7d4d015a0a3ed1ed00653f03eddade300771a
suSiuu <151724940+suSiuu@users.noreply.github.com>
2023-12-04 14:17:54 +0800

    Update install.js

--- install.js
+++ install.js
@@ -3,7 +3,7 @@
 const https = require("follow-redirects").https;
 const tar = require("tar");
 const shell = require("shelljs");
-const HttpsProxyAgent = require("https-proxy-agent");
+const { HttpsProxyAgent } = require("https-proxy-agent");
 const version = require("./package.json")["psc-package-version"];
 const platform = { win32: "win64", darwin: "macos" }[process.platform] || "linux64";
 
@@ -11,7 +11,7 @@ console.log(require('https-proxy-agent'),'require');
 
 // agent's address
 const proxy = 'http://172.23.240.1:7890';  // 替换为你的代理地址和端口
-const agent = HttpsProxyAgent.HttpsProxyAgent(proxy);
+const agent = new HttpsProxyAgent(proxy);
 
 https.get({
   hostname: 'github.com',
