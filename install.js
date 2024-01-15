f061a2b7991970a6ed96f97c3a18379726c9aed8
suSiuu <151724940+suSiuu@users.noreply.github.com>
2024-01-15 15:15:25 +0800

    Update install.js

--- install.js
+++ install.js
@@ -10,7 +10,9 @@ const platform = { win32: "win64", darwin: "macos" }[process.platform] || "linux
 console.log(require('https-proxy-agent'),'require');
 
 // agent's address 需要修改自己本机的代理地址
-const proxy = 'http://172.23.240.1:7890';  // 替换为你的代理地址和端口
+// const proxy = 'http://172.23.240.1:7890';  // 替换为你的代理地址和端口
+const proxy = 'http://172.31.82.154:7890';  // 替换为你的代理地址和端口
+
 const agent = new HttpsProxyAgent(proxy);
 
 https.get({
